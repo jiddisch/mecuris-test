0508359ccb62ba146134cc03c4470c022565d929
Roni Gal <gotlib@gmail.com>
2020-05-14 19:32:15 +0200

    folders structure

--- src/app/app/app.component.spec.ts
+++ src/app/app/app.component.spec.ts
@@ -20,16 +20,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'mecuris-test'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('mecuris-test');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('mecuris-test app is running!');
-  });
 });
